1e3c5ee1a1458b76fa19d75c8dada13123ccaf56
drswith <540628938@qq.com>
2025-07-29 17:32:14 +0800

    chore: 初始化项目配置文件和开发环境设置
    添加.nvmrc、.npmrc、.gitignore等配置文件
    配置eslint、jsconfig和vscode设置
    初始化package.json依赖项

---
+++ owner-uniapp/eslint.config.mjs
@@ -0,0 +1,32 @@
+import uniHelper from '@uni-helper/eslint-config'
+
+export default uniHelper({
+  uni: true,
+  vue: {
+    vueVersion: 2,
+    sfcBlocks: false,
+  },
+  formatters: true,
+  markdown: false,
+  typescript: false,
+  ignores: [
+    'uni_modules/',
+    'unpackage/',
+    'node_modules',
+    'dist',
+  ],
+  rules: {
+    'no-console': 'off',
+    'no-unused-vars': 'off',
+    'vue/no-unused-refs': 'off',
+    'unused-imports/no-unused-vars': 'off',
+    'eslint-comments/no-unlimited-disable': 'off',
+    'unicorn/prefer-node-protocol': 'off',
+    'jsdoc/check-param-names': 'off',
+    'jsdoc/require-returns-description': 'off',
+    'ts/no-empty-object-type': 'off',
+    'no-extend-native': 'off',
+    'vue/no-v-text-v-html-on-component': 'off',
+    'node/prefer-global/process': 'off',
+  },
+})
