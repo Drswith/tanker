042f23003481d25ed08dd9b5116ffaee313dac2d
drswith <540628938@qq.com>
2025-07-30 10:35:40 +0800

    feat(司机): 新增司机管理相关页面和路由配置
    添加司机列表、新增司机和司机详情页面基础结构
    更新pages.json配置司机相关路由

---
+++ owner-uniapp/pages/driver/detail/index.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  components: {
+
+  },
+  props: {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  watch: {
+
+  },
+  created() {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<template>
+  <div>
+    driver detail
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
