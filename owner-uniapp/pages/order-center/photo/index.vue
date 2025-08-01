d0702535dbcc12ecbcd00119aa3142d91faadf6c
drswith <540628938@qq.com>
2025-08-01 10:24:36 +0800

    feat(订单中心): 新增订单相关页面和路由配置
    添加验收授权、确认收货、订单进度、订单详情和照片信息页面

---
+++ owner-uniapp/pages/order-center/photo/index.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+  components: {
+
+  },
+  props: {
+
+  },
+  data() {
+    return {
+      // 路由参数
+      routeParams: {
+        orderId: null,
+        orderNo: '',
+        driverId: null,
+      },
+
+      // 订单数据
+      orderData: null,
+    }
+  },
+  computed: {
+
+  },
+  watch: {
+
+  },
+  onLoad(options) {
+    this.routeParams.orderId = options.id
+    this.routeParams.orderNo = options.orderNo || ''
+    this.routeParams.driverId = options.driverId || null
+
+    // 如果有传递的订单数据，直接使用
+    if (options.orderData) {
+      try {
+        this.orderData = JSON.parse(decodeURIComponent(options.orderData))
+        this.routeParams.orderNo = this.orderData.orderNo || ''
+        this.routeParams.driverId = this.orderData.driverId || null
+      }
+      catch (error) {
+        console.error('解析订单数据失败:', error)
+      }
+    }
+  },
+  created() {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<template>
+  <div>
+    photo
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
