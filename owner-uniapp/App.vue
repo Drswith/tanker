aa0f9820847de87b15218524f8a277e47f3fe100
Drswith <drswith@outlook.com>
2025-08-02 23:20:10 +0800

    feat: 添加全局变量os用于识别操作系统类型
    在App.vue中添加全局变量os来存储设备操作系统类型，并在应用启动时通过uni.getDeviceInfo()检测当前系统类型，支持识别iOS、Android和HarmonyOS系统

--- owner-uniapp/App.vue
+++ owner-uniapp/App.vue
@@ -9,6 +9,7 @@ const mapKey = 'D6ABZ-7O73L-UXXPG-E4RBT-S4GGE-5ABV5'
 
 export default {
   globalData: {
+    os: 'unknown',
     mapKey,
     // 实例化腾讯地图API核心类
     // #ifdef MP-WEIXIN
@@ -21,6 +22,27 @@ export default {
   },
   onLaunch() {
     console.log('App Launch')
+    const deviceInfo = uni.getDeviceInfo()
+    const isIos = deviceInfo.system.includes('iOS')
+    const isAndroid = deviceInfo.system.includes('Android')
+    const isHarmony = deviceInfo.system.includes('HarmonyOS')
+
+    console.warn('[SYSTEM] 终端信息', deviceInfo)
+    if (isIos) {
+      console.warn('[SYSTEM] Running at ios')
+      this.globalData.os = 'ios'
+    }
+    else if (isAndroid) {
+      console.warn('[SYSTEM] Running at android')
+      this.globalData.os = 'android'
+    }
+    else if (isHarmony) {
+      console.warn('[SYSTEM] Running at harmony')
+      this.globalData.os = 'harmony'
+    }
+    else {
+      console.warn('[SYSTEM] unknown os')
+    }
   },
   onShow() {
     console.log('App Show')
