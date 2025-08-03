4bd16919cd201cc018e4e40f194f5dc4230f77be
Drswith <drswith@outlook.com>
2025-08-03 15:57:22 +0800

    refactor(main.js): 重组初始化代码并添加lodash工具
    - 将代码按功能模块重新组织，提高可读性
    - 统一uView配置方式
    - 添加lodash工具作为Vue原型方法

--- owner-uniapp/main.js
+++ owner-uniapp/main.js
@@ -1,50 +1,48 @@
-import App from './App'
-// #ifndef VUE3
+// 引入lodash工具
+import lodash from 'lodash'
 import Vue from 'vue'
-import './uni.promisify.adaptor'
-Vue.config.productionTip = false
-App.mpType = 'app'
-const app = new Vue({
-	...App,
-})
-app.$mount()
-// #endif
 
-// #ifdef VUE3
-import {
-	createSSRApp
-} from 'vue'
-export function createApp() {
-	const app = createSSRApp(App)
-	return {
-		app
-	}
-}
-// #endif
+// 公共方法
+import $com from '@/common/js/com.js'
+// 公共时间方法
+import $time from '@/common/js/time.js'
 
 import uView from '@/uni_modules/uview-ui'
-Vue.use(uView)
-uni.$u.config.unit = 'rpx'
 
-//公共常量
-import constants from './common/js/constants'
-Vue.prototype.constants = constants;
-
-//接口
+// 接口
 import httpApi from '@/utils/httpApi.js'
-Vue.prototype.httpApi = httpApi;
+
+import App from './App'
+
+// 公共常量
+import constants from './common/js/constants'
+
+import './uni.promisify.adaptor'
 
 // 过滤器
-import "@/common/js/filter.js";
+import '@/common/js/filter.js'
 
-//公共方法
-import $com from '@/common/js/com.js'
-Vue.prototype.$com = $com;
+Vue.config.productionTip = false
 
-//公共时间方法
-import $time from '@/common/js/time.js'
-Vue.prototype.$time = $time;
+Vue.use(uView)
+uni.$u.setConfig({
+  // 修改$u.config对象的属性
+  config: {
+    // 修改默认单位为rpx，相当于执行 uni.$u.config.unit = 'rpx'
+    // 那么在用于传入组件大小参数为数值时，就默认为rpx，但这样全局配置会导致UI组件过小
+    // unit: 'rpx',
+  },
+})
 
-//引入lodash工具
-import lodash from 'lodash';
-Vue.prototype.$lodash = lodash;
+Vue.prototype.constants = constants
+Vue.prototype.httpApi = httpApi
+Vue.prototype.$com = $com
+Vue.prototype.$time = $time
+Vue.prototype.$lodash = lodash
+
+App.mpType = 'app'
+
+const app = new Vue({
+  ...App,
+})
+app.$mount()
