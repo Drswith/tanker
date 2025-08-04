7ba96e66ddc88bde1b6d43a0eb42b5208759457d
drswith <540628938@qq.com>
2025-08-04 14:30:06 +0800

    docs(pay): 完善支付和退款类型的枚举文档
    添加详细的JSDoc注释说明支付和退款类型的枚举值及其用法示例

--- owner-uniapp/api/pay.js
+++ owner-uniapp/api/pay.js
@@ -1,20 +1,44 @@
 import { request } from '@/utils/http'
 
 /**
- * 支付类型
+ * 支付类型枚举
+ * @typedef {object} PayTypeEnum
+ * @property {'WX'} WECHAT - 微信支付
+ * @property {'ZFB'} ALIPAY - 支付宝支付
+ * @property {'YHK'} BANK - 银行卡支付
+ *
+ * @example
+ * // 使用支付类型
+ * const paymentMethod = PayType.WECHAT; // 'WX'
+ * const isWechatPay = paymentMethod === PayType.WECHAT;
  */
 export const PayType = Object.freeze({
+  /** 微信支付 */
   WECHAT: 'WX',
+  /** 支付宝支付 */
   ALIPAY: 'ZFB',
+  /** 银行卡支付 */
   BANK: 'YHK',
 })
 
 /**
- * 退款类型
+ * 退款类型枚举
+ * @typedef {object} RefundTypeEnum
+ * @property {0} WECHAT - 微信退款
+ * @property {1} ALIPAY - 支付宝退款
+ * @property {2} BANK - 银行卡退款
+ *
+ * @example
+ * // 使用退款类型
+ * const refundMethod = RefundType.WECHAT; // 0
+ * const isWechatRefund = refundMethod === RefundType.WECHAT;
  */
 export const RefundType = Object.freeze({
+  /** 微信退款 */
   WECHAT: 0,
+  /** 支付宝退款 */
   ALIPAY: 1,
+  /** 银行卡退款 */
   BANK: 2,
 })
 
